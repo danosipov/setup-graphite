4ce24f3f03b06f97d7f57d77e57ac7d5ae09eae4
Nam Nguyen <namnguye@adobe.com>
2013-10-15 12:37:02 -0700

    Update to work with 0.9.12

--- ubuntu.bash
+++ ubuntu.bash
@@ -1,86 +1,75 @@
-# Prepare
-apt-get update
-apt-get -y upgrade
+function escapeSearchPattern
+{
+    echo "$(echo "${1}" | sed "s@\[@\\\\[@g")"
+}
 
-# Timezone
-cp /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
-echo America/Los_Angeles > /etc/timezone
-date
+function installDependencies
+{
+    apt-get -y install apache2
+    apt-get -y install erlang-os-mon
+    apt-get -y install erlang-snmp
+    apt-get -y install libapache2-mod-python
+    apt-get -y install libapache2-mod-wsgi
+    apt-get -y install memcached
+    apt-get -y install python-cairo-dev
+    apt-get -y install python-dev
+    apt-get -y install python-django
+    apt-get -y install python-ldap
+    apt-get -y install python-memcache
+    apt-get -y install python-pip
+    apt-get -y install python-pysqlite2
+    apt-get -y install rabbitmq-server
+    apt-get -y install sqlite3
 
-# Download Graphite
-rm -rf /tmp/graphite
-mkdir -p /tmp/graphite
-cd /tmp/graphite
+    pip install django-tagging
+}
 
-git clone https://github.com/graphite-project/carbon.git
-git clone https://github.com/graphite-project/graphite-web.git
-git clone https://github.com/graphite-project/whisper.git
+function installGraphite
+{
+    pip install carbon
+    pip install graphite-web
+    pip install whisper
+}
 
-# Check Dependencies
-cd /tmp/graphite/graphite-web
-python check-dependencies.py
+function configApache
+{
+    local oldWSGISocketPrefix="$(escapeSearchPattern 'WSGISocketPrefix run/wsgi')"
+    local newWSGISocketPrefix="$(escapeSearchPattern 'WSGISocketPrefix /var/run/apache2/wsgi')"
 
-# Install Dependencies
-apt-get -y install libcairo2-dev
+    sed "s@${oldWSGISocketPrefix}@${newWSGISocketPrefix}@g" '/opt/graphite/examples/example-graphite-vhost.conf' > '/etc/apache2/sites-available/default'
 
-apt-get -y install python-pip
-pip install Django==1.5.4
+    chown -R www-data:www-data '/opt/graphite/storage'
+}
 
-pip install django-tagging
+function configGraphite
+{
+    mv /opt/graphite/conf/carbon.conf.example /opt/graphite/conf/carbon.conf
+    mv /opt/graphite/conf/storage-schemas.conf.example /opt/graphite/conf/storage-schemas.conf
+    mv /opt/graphite/conf/graphite.wsgi.example /opt/graphite/conf/graphite.wsgi
 
-easy_install zope.interface
+    cd /opt/graphite/webapp/graphite
+    python manage.py syncdb
 
-apt-get -y install python-dev
-easy_install twisted
+    mv /opt/graphite/webapp/graphite/local_settings.py.example /opt/graphite/webapp/graphite/local_settings.py
+}
 
-apt-get -y install fontconfig
+function startServers
+{
+    /etc/init.d/apache2 restart
 
-apt-get -y install apache2 libapache2-mod-wsgi libapache2-mod-python
+    cd /opt/graphite
+    ./bin/carbon-cache.py start
+}
 
-apt-get -y install memcached
+function main
+{
+    installDependencies
+    installGraphite
 
-apt-get -y install python-memcache
+    configApache
+    configGraphite
 
-apt-get -y install python-ldap
+    startServers
+}
 
-easy_install txamqp
-
-apt-get -y install python-rrdtool
-
-apt-get -y install python-tz python-pyparsing python-cairo-dev 
-
-# Install
-cd /tmp/graphite/graphite-web
-python setup.py install
-
-cd /tmp/graphite/carbon
-python setup.py install
-
-cd /tmp/graphite/whisper
-python setup.py install
-
-# Check Dependencies
-cd /tmp/graphite/graphite-web
-python check-dependencies.py
-
-# Configure
-mv /opt/graphite/conf/carbon.conf.example /opt/graphite/conf/carbon.conf
-mv /opt/graphite/conf/storage-schemas.conf.example /opt/graphite/conf/storage-schemas.conf
-mv /opt/graphite/conf/graphite.wsgi.example /opt/graphite/conf/graphite.wsgi
-
-mv /etc/apache2/sites-available/default /etc/apache2/sites-available/default.bak
-cp /opt/graphite/examples/example-graphite-vhost.conf /etc/apache2/sites-available/default
-
-/etc/init.d/apache2 restart
-
-cd /opt/graphite/webapp/graphite
-python manage.py syncdb
-
-chown www-data:www-data /opt/graphite/storage/graphite.db
-mv /opt/graphite/webapp/graphite/local_settings.py.example /opt/graphite/webapp/graphite/local_settings.py
-
-mv /opt/graphite/conf/storage-aggregation.conf.example /opt/graphite/conf/storage-aggregation.conf
-
-mkdir -p /opt/graphite/storage/log/carbon-cache
-cd /opt/graphite
-./bin/carbon-cache.py start
+main "${@}"
